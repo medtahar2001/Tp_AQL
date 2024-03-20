103aeb1a98d7915b90473355580e9c1384c0bba1
zerrad <medtahar2001@gmail.com>
2024-03-20 01:06:24 +0100

    the second tp

---
+++ TP01/src/test/java/exercice1/PalindromeTest.java
@@ -0,0 +1,22 @@
+package exercice1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeTest {
+
+    @Test
+    void isPalindrome() {
+        assertTrue(Palindrome.isPalindrome("tahat"));
+    }
+    @Test
+    void isNotPalindrome() {
+        assertFalse(Palindrome.isPalindrome("taher"));
+    }
+    @Test
+    void StringNull(){
+
+
+    }
+}
